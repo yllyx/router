53423f931ac9dd00dfeade53e1e287b8ea04e003
yllyx <Yunxiang.Lon@gmail.com>
2018-01-28 17:52:32 +0800

    format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { Router } from '@angular/router';
 
-import { AppComponent }            from './app.component';
-import { AppRoutingModule }        from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
-import { HeroesModule }            from './heroes/heroes.module';
+import { HeroesModule } from './heroes/heroes.module';
 import { ComposeMessageComponent } from './compose-message.component';
-import { LoginRoutingModule }      from './login-routing.module';
-import { LoginComponent }          from './login.component';
-import { PageNotFoundComponent }   from './not-found/page-not-found.component';
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+import { PageNotFoundComponent } from './not-found/page-not-found.component';
 
-import { DialogService }           from './dialog.service';
+import { DialogService } from './dialog.service';
 
 @NgModule({
   imports: [
